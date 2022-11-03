cf2a09b4d0f8295ef4cfe8b1a972c930e7382c4d
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-11-03 11:07:56 +0200

    style tabs on attendance

--- src/components/tabs/attendanceTabs.tsx
+++ src/components/tabs/attendanceTabs.tsx
@@ -2,15 +2,38 @@ import { Tab } from "@headlessui/react";
 import ChildrenTable from "src/components/tables/attendance/children";
 import YouthsTable from "src/components/tables/attendance/youths";
 import AdultsTable from "src/components/tables/attendance/adults";
+import { useState } from "react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const AttendanceTabs = () => {
+  let [categories] = useState({
+    Children: 1,
+    Youths: 2,
+    Adults: 3,
+  });
   return (
     <div className=" flex flex-col  justify-center  w-full ">
       <Tab.Group>
-        <Tab.List className="text-center">
-          <Tab>Children</Tab>
-          <Tab>Youths</Tab>
-          <Tab>Adults</Tab>
+        <Tab.List className=" flex space-x-1 rounded-xl bg-blue-300/20 p-1">
+          {Object.keys(categories).map((category) => (
+            <Tab
+              className={({ selected }) =>
+                classNames(
+                  "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-black",
+                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-200 focus:outline-none focus:ring-2",
+                  selected
+                    ? "bg-white shadow"
+                    : "text-black hover:bg-white/[0.12] hover:text-white"
+                )
+              }
+            >
+              {" "}
+              {category}
+            </Tab>
+          ))}
         </Tab.List>
         <Tab.Panels>
           <Tab.Panel>
--- src/pages/attendance/index.tsx
+++ src/pages/attendance/index.tsx
@@ -4,7 +4,7 @@ import AttendanceTable from "src/components/tables/attendanceTable";
 const Attendance = () => {
   return (
     <div className=" w-4/6">
-      <h1 className="text-center">This is the attendance page</h1>
+      <h1 className="text-center py-5">This is the attendance page</h1>
       <AttendanceTabs />
     </div>
   );
